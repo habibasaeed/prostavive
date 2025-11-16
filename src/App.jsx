d14292d30e8a76917661e781606a79eb90d5279f
habibasaeed <100446114+habibasaeed@users.noreply.github.com>
2025-11-16 23:23:58 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -99,7 +99,7 @@ export default function App() {
         <div className="flex items-center justify-center relative">
           <div className="relative group">
             <img
-              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSgYIwRs-QNEuME90jIh8JVtFtN5dlSl-q9w&s"
+            src="src\prost.jpg"
               alt="ProstaVive bottle"
               className="w-72 md:w-96 rounded-2xl shadow-2xl transition-transform duration-500 group-hover:scale-105"
             />
